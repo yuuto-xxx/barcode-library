4592bea1df8d3b5bf36ff6bd94fa9b211b9ea5b0
komatsuyama yuuto <y.komatsuyama.sys20@morijyobi.ac.jp>
2021-11-02 10:48:23 +0900

    dbのテスト

--- db.py
+++ db.py
@@ -5,21 +5,22 @@ import random
 import hashlib
 import string
 
-#dbname=d146sdrtncr1rm 
-host = "ec2-23-23-199-57.compute-1.amazonaws.com "
+dbname="d146sdrtncr1rm"
+host = "ec2-23-23-199-57.compute-1.amazonaws.com"
 port = 5432 
-user = "sudfwfyugnjfdf "
-password = "46a5575767a9c88ebcb1930e8afe9c557df8911a3b4021ce902a500ba47a4e8b "
+user = "sudfwfyugnjfdf"
+password = "46a5575767a9c88ebcb1930e8afe9c557df8911a3b4021ce902a500ba47a4e8b"
 sslmode = "require"
 
 DATABASE_URL = os.environ.get('postgres://sudfwfyugnjfdf:46a5575767a9c88ebcb1930e8afe9c557df8911a3b4021ce902a500ba47a4e8b@ec2-23-23-199-57.compute-1.amazonaws.com:5432/d146sdrtncr1rm')
+url = "postgres://sudfwfyugnjfdf:46a5575767a9c88ebcb1930e8afe9c557df8911a3b4021ce902a500ba47a4e8b@ec2-23-23-199-57.compute-1.amazonaws.com:5432/d146sdrtncr1rm"
 
 def test():
-    conn = psycopg2.connect(DATABASE_URL, user=user, password=password)
+    conn = psycopg2.connect(database=dbname, user=user, password=password, host=host, port=port, sslmode='allow')
     cur = conn.cursor()
     print("DBアクセス")
     cur.execute("select * from course")
     result = cur.fetchall()
-    print("リザルト表示:" + result)
+    print(result)
     cur.close()
     conn.close()
