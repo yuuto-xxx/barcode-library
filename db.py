d431fb5b689bdb57bb075bbc84bfbf77a948e5c1
wwm263996 <b.ou.sys20@morijyobi.ac.jp>
2021-12-10 10:40:41 +0900

    タグ機能の追加

--- db.py
+++ db.py
@@ -414,6 +414,178 @@ def book_search(key):
 
     return result
 
+# タグのselect
+def select_tag(isbn):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    sql = "select * from tag join book_tag on (tag.tag_id=book_tag.tag_id) where book_isbn=%s"
+
+    try:
+        cur.execute(sql,(isbn,))
+    except Exception as e:
+        print("本のtag情報取得エラー")
+
+    result = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return result
+
+# タグのselect(詳細画面に表示)
+def select_tag_2(isbn):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    sql = "select tag.tag_name from tag join book_tag on (tag.tag_id=book_tag.tag_id) where book_isbn=%s"
+
+    try:
+        cur.execute(sql,(isbn,))
+    except Exception as e:
+        print("本のtag情報取得エラー")
+
+    result = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return result
+
+# タグのプルダウン
+def tag_pull_down(isbn):
+    old_tag = select_tag_2(isbn)
+    conn = get_connection()
+    cur = conn.cursor()
+
+    sql = "select tag_name from tag"
+
+    try:
+        cur.execute(sql,(isbn,))
+    except Exception as e:
+        print("本のtag情報取得エラー")
+
+    result = cur.fetchall()
+
+    cur.close()
+    conn.close()
+    for i in old_tag:
+        result.remove(i)
+    return result,old_tag
+
+
+# 本の検索(学生)
+def stu_book_search(key):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    sql = "select DISTINCT ON(book.title) * from book join book_tag on (book.book_isbn=book_tag.book_isbn) join tag on (tag.tag_id=book_tag.tag_id) where (book.title ilike %s or book.author ilike %s or book.publisher ilike %s or tag.tag_name ilike %s) and book.book_delete_flag = false"
+    key_like = "%"+ key +"%"
+    try:
+        cur.execute(sql,(key_like,key_like,key_like,key_like,))
+    except Exception as e:
+        print("(学生)本の検索取得エラー",e)
+
+    result = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return result
+
+# タグで本を検索
+def tag_book_search(key):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    sql = "select DISTINCT ON(book.title) * from book join book_tag on (book.book_isbn=book_tag.book_isbn) join tag on (tag.tag_id=book_tag.tag_id) where tag.tag_name ilike %s and book.book_delete_flag = false"
+
+    key_like = "%"+key+"%"
+    try:
+        cur.execute(sql,(key_like,))
+    except Exception as e:
+        print("タグで本の検索取得エラー",e)
+
+    result = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return result
+
+# タグの追加
+def tag_add_book(isbn,tag):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    tag_id = select_tag_id(tag)
+    print(isbn)
+    print(tag_id)
+    if tag_id:
+        sql = "insert into book_tag(book_isbn,tag_id) values(%s,%s)"
+        try:
+            cur.execute(sql,(isbn,tag_id))
+        except Exception as e:
+            print("タグ追加エラー",e)
+
+        conn.commit()
+        cur.close()
+        conn.close()
+        return True
+    else :
+        tag_add(tag)
+        tag_id = select_tag_id(tag)
+        sql = "insert into book_tag(book_isbn,tag_id) values(%s,%s)"
+        try:
+            cur.execute(sql,(isbn,int(tag_id)))
+        except Exception as e:
+            print("タグ追加エラー(else)",e)
+
+        conn.commit()
+        cur.close()
+        conn.close()
+        return True
+
+ 
+# タグIDのselect
+def select_tag_id(tag_name):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    sql = "select tag_id from tag where tag_name= %s"
+
+    try:
+        cur.execute(sql,(tag_name,))
+    except Exception as e:
+        print("タグID検索エラー",e)
+
+    result = cur.fetchone()
+
+    cur.close()
+    conn.close()
+    if result:
+        return result[0]
+    else :
+        return None
+
+# タグテーブルのタグ追加
+def tag_add(tag):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    sql = "insert into tag(tag_name) values (%s);"
+
+    try:
+        cur.execute(sql,(tag,))
+    except Exception as e:
+        print("タグテーブルタグinsertエラー", e)
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return True
+
 # 本の情報変更
 def book_change(isbn,title,author,pub,day,num):
 #     result = db.book_change(book[0],title,author,pub,day,num)
@@ -481,7 +653,7 @@ def promotion_student_list():
         join course on (student.course_id=course.course_id) where \
             (((student.course_id='3' or student.course_id='10') and year=3) \
             or (student.course_id='4' and year=4) \
-            or (student.course_id<>'3' and student.course_id<>'4' and student.course_id<>'10' and year=2))and delete_flag=false;"
+            or (student.course_id<>'3' and student.course_id<>'4' and student.course_id<>'10' and year=2))and student.delete_flag=false;"
 
     try:
         cur.execute(sql,)
@@ -673,7 +845,7 @@ def student_search_change_result(stu_number):
     sql = "select student.mail,student.name,student.stu_number,\
         course.course_name,student.year from student \
             join course on (student.course_id = course.course_id) \
-                where stu_number = %s and delete_flag is false"
+                where student.stu_number = %s and student.delete_flag is false"
 
     try:
         cur.execute(sql,(stu_number,))
